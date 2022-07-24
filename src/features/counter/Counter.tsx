109f3d21234b0e4cbead8dbf554c7b496c017d1e
hiraoku.shinichi <hiraoku.shinichi@MacBook-Pro-hiraoku.local>
2022-07-24 19:59:40 +0900

    first commit

---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,29 @@
+import { decrement, increment } from "./counterSlice";
+import styles from "./Counter.module.scss";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+
+export function Counter() {
+  // The `state` arg is correctly typed as `RootState` already
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <div>
+        <h1>Count: {count}</h1>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/features/counter/counterSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+// Define a type for the slice state
+type CounterState = {
+  value: number;
+};
+
+// Define the initial state using that type
+const initialState: CounterState = {
+  value: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export default counterSlice.reducer;
